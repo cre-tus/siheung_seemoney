1d4c2581757c7abe7a6f5f8acdd066e5a7f468ce
JINYOUNG YUN <pineappleboy@tukorea.ac.kr>
2026-05-10 22:31:52 +0900

    회원가입 버튼 누를 시 백엔드 전송

--- android/app/src/main/java/com/example/siheung_seemoney/data/network/RetrofitClient.kt
+++ android/app/src/main/java/com/example/siheung_seemoney/data/network/RetrofitClient.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 
 /**
@@ -40,6 +41,7 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
+            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
